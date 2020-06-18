9f7f0a178cedcee4ad777f84ada69cc39af2fefc
remoo1901 <remoo1901@hotmail.com>
2020-06-17 21:38:17 -0400

    Finished GET request

--- api/server.js
+++ api/server.js
@@ -6,4 +6,35 @@ const server = express();
 
 server.use(express.json());
 
+//-----------------------
+// GET Accounts
+//-----------------------
+
+server.get("/api/accounts", async (req, res, next) => {
+  try {
+    const data = await db.select("*").from("accounts");
+    
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//-----------------------
+// GET Accounts by ID
+//-----------------------
+
+server.get("/api/accounts/:id", async (req, res, next) => {
+  try {
+    const data = await db
+      .select("*")
+      .from("accounts")
+      .where("id", req.params.id);
+
+    res.status("200").json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = server;
